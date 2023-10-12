ffec30c0fa8df0ed432fc25ef77cd5aada842841
Jake Siney <jakesiney@gmail.com>
2023-10-12 16:05:25 +0100

    Application no sniff

--- login_form/__init__.py
+++ login_form/__init__.py
@@ -11,10 +11,12 @@ csrf = CSRFProtect()
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SESSION_COOKIE_SAMESITE'] = 'lax'
     csrf.init_app(app)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('KEY'),
         DATABASE=os.path.join(app.instance_path, 'login_form.sqlite'),
+
     )
 
     if test_config is None:
@@ -43,7 +45,9 @@ def create_app(test_config=None):
 
     @app.after_request
     def add_security_headers(resp):
-        resp.headers['Content-Security-Policy'] = "default-src 'self'"
+        csp = "default-src 'self'; frame-ancestors 'self'; form-action 'self'"
+        resp.headers['Content-Security-Policy'] = csp
+        resp.headers['X-Content-Type-Options'] = 'nosniff'
         return resp
 
     return app
